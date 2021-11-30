218b64a96999ae76f59f31aa8671b78925e6a122
FisehaTessema4 <60725293+FisehaTessema4@users.noreply.github.com>
2021-11-29 20:20:10 -0800

    Added some lines of code
    Updated the code

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 # This is the template code for the CNA337 Final Project
 # Zachary Rubin, zrubin@rtc.edu
 # CNA 337 Fall 2020
-
+import Server
 def print_program_info():
     # TODO - Change your name
-    print("Server Automator v0.1 by Your Name")
+    print("Fiseha Tessema")
 
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
     # TODO - Create a Server object
     # TODO - Call Ping method and print the results
+    #The IP address is passed as a string to the Server class in the server.py module
+    ec2Server = Server.Server("52.41.99.1")
